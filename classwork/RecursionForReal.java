2f6833fa04813fb9be1c98d53735df99cf05b071
mayawastaken <mayadoronrepa@gmail.com>
2022-02-09 14:51:02 -0500

    RecursionClasswork did groupSum6, it works with tests

--- classwork/RecursionForReal.java
+++ classwork/RecursionForReal.java
@@ -6,15 +6,40 @@ public class RecursionForReal{
     return (target == 0);
   }
 
+  public static boolean groupSum6(int start, int[] nums, int target){
+    if (start < nums.length && target != 0){
+      if (nums[start] == 6){
+        return (groupSum6(start+1, nums, target - nums[start]));
+      }
+      else{
+        return (groupSum6(start+1, nums, target - nums[start]) || groupSum6(start+1, nums, target));
+      }
+    }
+    if (start < nums.length){
+      if (nums[start] == 6){
+        return false;
+      }
+      else{
+        return groupSum6(start+1, nums, target);
+      }
+    }
+    return (target == 0);
+  }
+
 
   public static void main(String[] args){
     int[] test1 = {2,4,8};
     int[] test2 = {2,3,7,9};
+    int[] test3 = {5,6,2};
+    System.out.println("groupSum----------");
     System.out.println(groupSum(0, test1, 10));
     System.out.println(groupSum(0, test1, 14));
     System.out.println(groupSum(0, test1, 9));
     System.out.println(groupSum(0, test2, 12));
-
-
+    System.out.println("groupSum6---------");
+    System.out.println(groupSum(0, test3, 8));
+    System.out.println(groupSum(0, test3, 9));
+    System.out.println(groupSum(0, test3, 10));
+    System.out.println(groupSum(0, test3, 11));
   }
 }
