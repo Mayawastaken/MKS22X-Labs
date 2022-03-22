949e9d227348eba2bf2a982d98b27a832286d27a
mayawastaken <mayadoronrepa@gmail.com>
2022-03-22 14:38:34 -0400

    onto mergesort

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class Merge{     // know rutime of stuff and why, knw when u cant do stuff like data[0] = smth (ex in for each or 2d arr or 1d arr)
   public static int [] merge(int [] left, int[] right){
     int[] combined = new int[left.length+right.length];
-    //int minLen = Math.min(left.length, right.length);
     int leftInd = 0, rightInd = 0, combinedInd = 0;
     while (leftInd < left.length && rightInd < right.length){
       if (left[leftInd] < right[rightInd]){
@@ -18,8 +17,6 @@ public class Merge{     // know rutime of stuff and why, knw when u cant do stuf
       }
       combinedInd++;
     }
-    System.out.println(Arrays.toString(combined)); //up till here we good hm
-    System.out.println("left ind: " + leftInd + "\nright ind: " + rightInd);
     if (leftInd == left.length){
       for (int i = rightInd; i < right.length; i++){
         int temp = right[i];
@@ -33,7 +30,6 @@ public class Merge{     // know rutime of stuff and why, knw when u cant do stuf
         combined[combinedInd] = temp;
         combinedInd++;
       }
-      //add rest of left and make sure it acc transfers data w ar[i] = ar2[i2] like is it the acc value or smth else
     }
     return combined;
     //return a new array that is the merged version of left and right
@@ -54,8 +50,8 @@ public class Merge{     // know rutime of stuff and why, knw when u cant do stuf
   }
 
   public static void main(String[] args){
-    int[] a1 = new int[]{0,6,7,8,9};
-    int[] a2 = new int[]{1,2,3,4, 10};
+    int[] a1 = new int[]{0,6,7,8,9,11,13};
+    int[] a2 = new int[]{1,2,3,4,10};
     System.out.println(Arrays.toString(merge(a1,a2)));
 
 
