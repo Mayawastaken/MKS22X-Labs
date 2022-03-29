a5b48e094130ba05e6e812ea928e28d682e45496
mayawastaken <mayadoronrepa@gmail.com>
2022-03-29 14:50:33 -0400

    uhh debug ahah..

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -10,8 +10,8 @@ public class MyDeque<E>{
     E[] d = (E[])new Object[10];
     data = d;
     size = 0;
-    start = 10/2;
-    end = 10/2 + 1;
+    start = 0;
+    end = 0;
   }
 
   public MyDeque(int initialCapacity){ //NOTE: assumes size at least 2 bcs start n end, check if I need to worry ab that
@@ -19,8 +19,8 @@ public class MyDeque<E>{
     E[] d = (E[])new Object[initialCapacity];
     data = d;
     size = 0;
-    start = initialCapacity/2;
-    end = initialCapacity/2 + 1;
+    start = 0;
+    end = 0;
   }
 
   public int size(){
@@ -29,17 +29,75 @@ public class MyDeque<E>{
 
   private void resize(){
     @SuppressWarnings("unchecked")
-    E[] d = (E[])new Object[size + 20]; //assuming we have a checker to see if size hits capacity
+    E[] d = (E[])new Object[size * 2 + 1]; //assuming we have a checker to see if size hits capacity
     int dind = 0;
     if (end < start){
       for (int i = start; i < data.length; i++){
-        d[dind + 10] = data[i];
-      } //wait i need to draw this out -- do later
+        d[dind] = data[i];
+        dind++;
+      }
+      for (int i = 0; i <= end; i++){
+        d[dind] = data[i];
+        dind++;
+      }
     }
+    else if (end > start){
+      for (int i = 0; i <= end; i++){
+        d[dind] = data[i];
+      }
+    }
+    //um what if end = start like 0 elements 0 size?
 
   }
+  //ugh toString debug here we go
 
-  public String toString(){ }
+  public String  toStringDebug(){
+    String s = "[";
+    for (int i = 0; i < data.length; i++){
+      if (i != data.length-1){
+        s += data[i] + ", ";
+      }
+      else{
+        s += data[i];
+      }
+    }
+    return s + "]";
+  }
+
+  public String toString(){
+    String s = "[";
+    if (start == end){
+      return "[]";
+    }
+    int dind = 0;
+    if (start < end){
+      for (int i = start; i <= end; i++){
+        if (i != end){
+          s += data[i] + ", ";
+        }
+        else{
+          s+= data[i] + "]";
+        }
+      }
+    }
+    else if (start > end){
+      for (int i = start; i < data.length; i++){
+        s += data[dind] + ", ";
+        dind++;
+      }
+      for (int i = 0; i <= end; i++){
+        if (i != end){
+          s += data[dind] + ", ";
+          dind++;
+        }
+        else{
+          s+= data[dind] + "]";
+          dind++;
+        }
+      }
+    }
+    return s;
+  }
 
 
   public void addFirst(E element){
@@ -82,16 +140,64 @@ public class MyDeque<E>{
   }
 
 
-  public E removeFirst(){ }
+  public E removeFirst(){
+    if (size == 0){
+      throw new NoSuchElementException("no elements in deque to remove first");
+    }
+    E removed = data[start];
+    data[start] = null;
+    size--;
+    if (size != 1 && start == data.length-1){
+      start = 0;
+    }
+    else{
+      start++;
+    } //bcs end will always be going ---> ie will never wrap around or uh
+    return removed;
+  }
 
 
-  public E removeLast(){ }
+  public E removeLast(){
+    if (size == 0){
+      throw new NoSuchElementException("no elements in deque to remove last");
+    }
+    E removed = data[end];
+    data[end] = null;
+    size--;
+    if (size != 1 && end == 0){
+      end = size-1;
+    }
+    else{
+      end--;
+    } //bcs end will always be going ---> ie will never wrap around uhh
+    return removed;
+  }
 
 
-  public E getFirst() throws NoSuchElementException{
+  public E getFirst(){
+    if (size == 0){
+      throw new NoSuchElementException("no elments in deque to get first");
+    }
     return data[start];
   }
-  public E getLast() throws NoSuchElementException{
+  public E getLast(){
+    if (size == 0){
+      throw new NoSuchElementException("no elments in deque to get first");
+    }
     return data[end];
+  }
+
+  public static void main(String[] args){
+    MyDeque<Integer> test1 = new MyDeque<Integer>(5);
+    System.out.println(test1.toString());
+    test1.addLast(0);
+    System.out.println(test1.toString()); //hmm it adds a null.. also what to do when start = end for like all of them? the start off is weird
+    System.out.println(test1.toStringDebug());
+    test1.addLast(1);
+    System.out.println(test1.toString());
+    System.out.println(test1.toStringDebug());
+    test1.addFirst(2);
+    System.out.println(test1.toString()); //oh lord
+    System.out.println(test1.toStringDebug());
   }
 }
